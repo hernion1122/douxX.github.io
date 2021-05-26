1da98002fe3a41bcbed9a34865a5c467acf60e81
Denis <ravenskidenis@bk.ru>
2021-05-27 01:47:05 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+(function ($) {
+	// $('.fade').slick({
+	// 	dots: true,
+	// 	infinite: true,
+	// 	speed: 500,
+	// 	fade: true,
+	// 	cssEase: 'linear'
+	// });
+
+
+	$('.js-hamburger').click(function () {
+		$(this).toggleClass('is-active');
+	});
+
+})(jQuery)
+
